92808621cdb3fd0fc435e707db72b5007d432236
chenyihsiung <swqys773@gmail.com>
2016-10-19 20:35:14 +0800

    first commit

---
+++ homework/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pushme = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'pushme
+        '
+        Me.pushme.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.pushme.Location = New System.Drawing.Point(22, 31)
+        Me.pushme.Name = "pushme"
+        Me.pushme.Size = New System.Drawing.Size(75, 23)
+        Me.pushme.TabIndex = 0
+        Me.pushme.Text = "push"
+        Me.pushme.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(875, 584)
+        Me.Controls.Add(Me.pushme)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pushme As System.Windows.Forms.Button
+
+End Class
