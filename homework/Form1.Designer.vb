2346f81101223982fcf75dd83bde18b69fa54ddd
chenyihsiung <swqys773@gmail.com>
2016-10-19 20:48:08 +0800

    Third commit

--- homework/Form1.Designer.vb
+++ homework/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.pushme = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'pushme
@@ -35,11 +36,21 @@ Partial Class Form1
         Me.pushme.Text = "push"
         Me.pushme.UseVisualStyleBackColor = True
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(185, 87)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Bu"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(875, 584)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.pushme)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -47,5 +58,6 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents pushme As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
